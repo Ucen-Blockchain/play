8fbe3b5bf811d421785f26f81cec91d6a034a08e
Bobinson K B <bobinson@gmail.com>
2018-04-15 17:15:16 +0530

    market+history, private_messages

--- libraries/plugins/market_history/market_history_plugin.cpp
+++ libraries/plugins/market_history/market_history_plugin.cpp
@@ -1,14 +1,14 @@
-#include <steemit/market_history/market_history_api.hpp>
+#include <ucenio/market_history/market_history_api.hpp>
 
-#include <steemit/chain/database.hpp>
-#include <steemit/chain/index.hpp>
-#include <steemit/chain/operation_notification.hpp>
+#include <ucenio/chain/database.hpp>
+#include <ucenio/chain/index.hpp>
+#include <ucenio/chain/operation_notification.hpp>
 
-namespace steemit { namespace market_history {
+namespace ucenio { namespace market_history {
 
 namespace detail {
 
-using steemit::protocol::fill_order_operation;
+using ucenio::protocol::fill_order_operation;
 
 class market_history_plugin_impl
 {
@@ -215,6 +215,6 @@ uint32_t market_history_plugin::get_max_history_per_bucket() const
    return _my->_maximum_history_per_bucket_size;
 }
 
-} } // steemit::market_history
+} } // ucenio::market_history
 
-STEEMIT_DEFINE_PLUGIN( market_history, steemit::market_history::market_history_plugin )
+STEEMIT_DEFINE_PLUGIN( market_history, ucenio::market_history::market_history_plugin )
--- libraries/plugins/private_message/include/steemit/private_message/private_message_operations.hpp
+++ libraries/plugins/private_message/include/steemit/private_message/private_message_operations.hpp
@@ -1,8 +1,8 @@
 
 #pragma once
 
-#include <steemit/protocol/base.hpp>
-#include <steemit/protocol/types.hpp>
+#include <ucenio/protocol/base.hpp>
+#include <ucenio/protocol/types.hpp>
 
 #include <fc/reflect/reflect.hpp>
 
@@ -10,9 +10,9 @@
 #include <string>
 #include <vector>
 
-namespace steemit { namespace private_message {
+namespace ucenio { namespace private_message {
 
-struct private_message_operation : public steemit::protocol::base_operation
+struct private_message_operation : public ucenio::protocol::base_operation
 {
     protocol::account_name_type  from;
     protocol::account_name_type  to;
@@ -27,7 +27,7 @@ typedef fc::static_variant< private_message_operation > private_message_plugin_o
 
 } }
 
-FC_REFLECT( steemit::private_message::private_message_operation, (from)(to)(from_memo_key)(to_memo_key)(sent_time)(checksum)(encrypted_message) )
+FC_REFLECT( ucenio::private_message::private_message_operation, (from)(to)(from_memo_key)(to_memo_key)(sent_time)(checksum)(encrypted_message) )
 
-DECLARE_OPERATION_TYPE( steemit::private_message::private_message_plugin_operation )
-FC_REFLECT_TYPENAME( steemit::private_message::private_message_plugin_operation )
+DECLARE_OPERATION_TYPE( ucenio::private_message::private_message_plugin_operation )
+FC_REFLECT_TYPENAME( ucenio::private_message::private_message_plugin_operation )
