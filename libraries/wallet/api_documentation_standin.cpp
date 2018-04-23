ac637479b3aa635d64dfe30160caacda38609309
Bobinson K B <bobinson@gmail.com>
2018-04-23 13:56:37 +0530

    fixing doxygen errors

--- libraries/wallet/api_documentation_standin.cpp
+++ libraries/wallet/api_documentation_standin.cpp
@@ -23,10 +23,10 @@
  */
 #include <iomanip>
 #include <boost/algorithm/string/join.hpp>
-#include <steemit/wallet/wallet.hpp>
-#include <steemit/wallet/api_documentation.hpp>
+#include <ucenio/wallet/wallet.hpp>
+#include <ucenio/wallet/api_documentation.hpp>
 
-namespace steemit { namespace wallet {
+namespace ucenio { namespace wallet {
    namespace detail {
       namespace
       {
@@ -88,4 +88,4 @@ namespace steemit { namespace wallet {
                 std::inserter(method_descriptions, method_descriptions.end()));
    }
 
-} } // end namespace steemit::wallet
+} } // end namespace ucenio::wallet
